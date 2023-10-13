1ce42d35935b1a4411f9d87f84277a9e62c08a3b
luaa abaza <43247117+lulu-abaza@users.noreply.github.com>
2023-10-13 15:13:48 +0300

    Add files via upload

---
+++ Exceptions.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.human;
+
+/**
+ *
+ * @author connect
+ */
+public class Exceptions {
+    public static void main(String[ ] args)  {
+        try {
+            System.out.println(5 / 0);
+        } catch (Exception e) {
+            System.out.println(5 + 0);
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -1,11 +1,25 @@
-package testPolymorphism;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.human;
+import java.util.Scanner;
 
+/**
+ *
+ * @author connect
+ */
 public class Main {
-    public static void main(String[] args) {
-        Animal animal = new Dog();
-        animal.makeSound();
-
-        Animal animal1 = new Cat();
-        animal1.makeSound();
+    static void checkAge() {
+        Scanner myObj = new Scanner(System.in);
+        int age = myObj.nextInt();
+        if(age < 18) {
+            throw new ArithmeticException("access denied");
+        } else {
+            System.out.println("access granted");
+        }
     }
+      public static void main(String[ ] args) {
+          checkAge();
+      }
 }
